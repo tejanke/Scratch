bdbc66e4cb0c597a1eb5bb1c1aea54636513b9d3
tejanke <62154969+tejanke@users.noreply.github.com>
2020-12-20 21:49:26 -0500

    build new_deck and shuffle functions

--- API/play-cards.py
+++ API/play-cards.py
@@ -1,1 +1,27 @@
-print("test")
+import requests
+
+def new_deck():
+    try:
+        r = requests.get("https://deckofcardsapi.com/api/deck/new/")
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return "Error {}".format(r.status_code)
+    except:
+        return "Error"
+
+def shuffle(id):
+    try:
+        url = "https://deckofcardsapi.com/api/deck/" + id + "/shuffle/"
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return "Error {}".format(r.status_code)
+    except:
+        return "Error"
+
+deck = new_deck()
+print(deck)
+shuf = shuffle(deck['deck_id'])
+print(shuf)
