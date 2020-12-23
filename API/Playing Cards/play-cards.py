6a043b4b71965433b9fb6786913d78d37bdddcb2
tejanke <62154969+tejanke@users.noreply.github.com>
2020-12-22 21:13:23 -0500

    add reshuffle function

--- API/Playing Cards/play-cards.py
+++ API/Playing Cards/play-cards.py
@@ -36,6 +36,17 @@ def draw(id, count):
     except Exception as e:
         return e
 
+def reshuffle(id):
+    try:
+        url = "https://deckofcardsapi.com/api/deck/" + id + "/shuffle/"
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return "Error {}".format(r.status_code)
+    except Exception as e:
+        return e
+
 deck = new_deck()
 print(deck)
 shuf = shuffle(deck['deck_id'])
@@ -43,4 +54,5 @@ print(shuf)
 d = draw(deck['deck_id'], 2)
 print(d)
 print("*" * 50)
-
+re = reshuffle(deck['deck_id'])
+print(re)
