423164b7dbaf13fcfb5c3310f468654e59abbb2c
tejanke <62154969+tejanke@users.noreply.github.com>
2020-12-26 18:44:17 -0500

    start emoji layout

--- API/Playing Cards/play-cards.py
+++ API/Playing Cards/play-cards.py
@@ -1,8 +1,33 @@
 # http://deckofcardsapi.com/
 # https://pypi.org/project/emoji/
 
+from emoji.core import emojize
 import requests
 import emoji
+
+card_emoji_values = {
+    "1" : ":one:",
+    "2" : ":two:",
+    "3" : ":three:",
+    "4" : ":four:",
+    "5" : ":five:",
+    "6" : ":six:",
+    "7" : ":seven:",
+    "8" : ":eight:",
+    "9" : ":nine:",
+    "10" : ":keycap_ten:",
+    "JACK" : ":jack_o_lantern:",
+    "KING" : ":santa:",
+    "QUEEN" : ":bride_with_veil:",
+    "ACE" : ":a:"
+}
+
+card_emoji_suits = {
+    "DIAMONDS" : ":diamonds:",
+    "CLUBS" : ":clubs:",
+    "HEARTS" : ":hearts:",
+    "SPADES" : ":spades:"
+}
 
 def new_deck():
     try:
@@ -56,3 +81,17 @@ print(d)
 print("*" * 50)
 re = reshuffle(deck['deck_id'])
 print(re)
+print(emoji.emojize(':thumbs_up:'))
+d = draw(deck['deck_id'], 1)
+print(d)
+for k,v in d.items():
+    print(k, v)
+for card in d['cards']:
+    val = card_emoji_values[card['value']]
+    sui = card_emoji_suits[card['suit']]
+    print(val, type(val))
+    print(sui, type(sui))
+    print(card['value'], card_emoji_values[card['value']])
+    print(card['suit'], card_emoji_suits[card['suit']])
+    print(emoji.emojize(val, use_aliases=True))
+    print(emoji.emojize(sui, use_aliases=True))
