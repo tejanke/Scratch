e96414fecad419703a501ed97cc0fe9654a86a8f
tejanke <62154969+tejanke@users.noreply.github.com>
2021-01-01 09:31:40 -0500

    add some scan logic

--- Networking/Port Scanner/scanner.py
+++ Networking/Port Scanner/scanner.py
@@ -15,3 +15,31 @@ What scan do you want to run?
 \t3) UDP Scan
 """)
 print(scan)
+
+if scan == '1':
+    print("nmap version: {}".format(scanner.nmap_version()))
+    scanner.scan(address, '400-500', '-v -sT')
+    print(scanner.scaninfo())
+    print(type(scanner.scaninfo()))
+    print("IP {} is {}".format(address, scanner[address].state()))
+    print(scanner[address].all_protocols())
+    print("Open ports {}".format(scanner[address]['tcp'].keys()))
+if scan == '2':
+    print("nmap version: {}".format(scanner.nmap_version()))
+    scanner.scan(address, '400-500', '-v -sS')
+    print(scanner.scaninfo())
+    print(type(scanner.scaninfo()))
+    print("IP {} is {}".format(address, scanner[address].state()))
+    print(scanner[address].all_protocols())
+    print("Open ports {}".format(scanner[address]['tcp'].keys()))
+if scan == '3':
+    print("nmap version: {}".format(scanner.nmap_version()))
+    scanner.scan(address, '400-500', '-v -sU')
+    print(scanner.scaninfo())
+    print(type(scanner.scaninfo()))
+    print("IP {} is {}".format(address, scanner[address].state()))
+    print(scanner[address].all_protocols())
+    print("Open ports {}".format(scanner[address]['udp'].keys()))
+else:
+    print("{} is not an option".format(scan))
+    exit
